e663508b65f9220726123c03fe0baa0e729aadc5
Bielka Cordero <bielka.corderohuerta.2022@student.lhschools.org>
2019-09-26 10:31:02 -0400

    implemented if else ladder

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -4,25 +4,42 @@ public class Main {
 
     public static void main(String[] args) {
 
-    //todo declare variables a,b,c,discriminant, axisOfSymmetry,parabolaFacing, x, y
-    double a,b,c,discriminant,axisOfSymmetry,x,y;
-    String parabolaFacing;
+        //todo declare variables a,b,c,discriminant, axisOfSymmetry,parabolaFacing, x, y
+        double a, b, c, discriminant, axisOfSymmetry, x, y;
+        String parabolaFacing;
 
-    //todo initialize manually
-    a=5;
-    b=3;
-    c=1;
+        //todo initialize manually
+        a = 5;
+        b = 3;
+        c = 1;
 
-    //todo do calculations
-    axisOfSymmetry = -b/(2*a);
-    discriminant = (b*b)-4*(a*c);
+        //todo do calculations
+        axisOfSymmetry = -b / (2 * a);
+        discriminant = (b * b) - 4 * (a * c);
+        if (discriminant > 0)
+            System.out.println("There are two real solutions ");
+    }
+    else if (discriminant ==0){
 
-    //todo print results
-    System.out.println("Axis of Symmetry: "+axisOfSymmetry);
-    System.out.println("Discriminant: "+discriminant);
+
+    {
+        System.out.println("There is one real solutions");
+    }
+    else{
+
+    {
+        System.out.println("There are NO real solutions");
+    }
+
+
+
+        //todo print results
+        System.out.println("Axis of Symmetry: " + axisOfSymmetry);
+        System.out.println("Discriminant: " + discriminant);
+
 
     //todo not in code check results
-        
+
 
     //todo initialize scanner and redo the initialize area
 
