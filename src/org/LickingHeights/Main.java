adbd5a0e29714fdfeb01bad65edd7a4cc6d28313
Bielka Cordero <bielka.corderohuerta.2022@student.lhschools.org>
2019-09-26 09:51:28 -0400

    added todo steps

---
+++ src/org/LickingHeights/Main.java
@@ -0,0 +1,53 @@
+package org.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    //todo declare variables a,b,c,discriminant, axisOfSymmetry,parabolaFacing, x, y
+
+    //todo initialize manually
+
+    //todo do calculations
+
+    //todo print results
+
+    //todo not in code check results
+
+    //todo initialize scanner and redo the initialize area
+
+    //todo double check results     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
